64b5e839f11d069ace77c275daa579a00eed4333
yosh1ba <hk.r8.718@gmail.com>
2021-02-27 00:01:35 +0900

    STEP 4-2 feched icon size

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,6 +4,10 @@ import Footer from "../components/footer"
 
 import "./layout.css"
 
+import "@fortawesome/fontawesome-svg-core/styles.css"
+import { config } from "@fortawesome/fontawesome-svg-core"
+config.autoAddCss = false
+
 export default ( {children} ) => (
   <div>
     <Header />
