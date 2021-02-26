047d8a42ace80fd2af82ecb0c7ed08a2a03dfbf9
yosh1ba <hk.r8.718@gmail.com>
2021-02-27 00:25:58 +0900

    STEP 4-4

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,6 +4,8 @@ import Footer from "../components/footer"
 
 import "./layout.css"
 
+/* ページ表示時にアイコンが拡大表示されるのを防ぐ。
+FontAwesomeのcssを先に読み込むことで回避する。 */
 import "@fortawesome/fontawesome-svg-core/styles.css"
 import { config } from "@fortawesome/fontawesome-svg-core"
 config.autoAddCss = false
